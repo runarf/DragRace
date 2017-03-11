8b35c7802ae671ce3ac36d059fa1c37aa62b4c54
Runar Fra <runar@tuta.io>
2017-03-11 14:32:23 +0100

    First commit

---
+++ Assets/Note.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = new Vector2 (0, -speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
